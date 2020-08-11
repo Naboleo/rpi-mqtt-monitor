621f42ff4f30835f564ac114fa03a5575a5ffb1f
Naboleo <32004297+Naboleo@users.noreply.github.com>
2020-08-11 21:48:32 +0200

    Many changes
    - JSON format
    - Optimized syscalls
    -Simplify

--- src/config.py
+++ src/config.py
@@ -5,12 +5,8 @@ mqtt_host = "192.168.1.49"
 mqtt_user = "jeedom"
 mqtt_password = "cc2531"
 mqtt_port = "1883"
-mqtt_topic_prefix = "Jeedom Monitor"
-
 # Messages configuration
-
-# If this is set to True the script will send just one message containing all values 
-group_messages = False
+mqtt_topic_prefix = "pi2mqtt"
 
 # Random delay in seconds before measuring the values 
 # - this is used for de-synchronizing message if you run this script on many hosts, set this to 0 for no delay.
@@ -18,14 +14,15 @@ group_messages = False
 random_delay = randrange(30)
 
 # This is the time  between sending the indivudual messages
-sleep_time = 0.5
+sleep_time = 1
 cpu_load = True
 cpu_temp = True
 used_space = True
 voltage = True
-sys_clock_speed = False
+sys_clock_speed = True
 swap = False
 memory = True
 cpu_average = True
-cores = False
+cores = True
 uptime = True
+throttled = True
--- src/rpi-cpu2mqtt.py
+++ src/rpi-cpu2mqtt.py
@@ -1,160 +1,138 @@
-# Python 2 script to check cpu load, cpu temperature and free space,
+# Python 3 script to check cpu load, cpu temperature and free space,
 # on a Raspberry Pi computer and publish the data to a MQTT server.
 # RUN pip install paho-mqtt
 # RUN sudo apt-get install python-pip
 
-from __future__ import division
 import subprocess, time, socket, os
 import paho.mqtt.client as paho
 import json
 import config
 
-# get device host name - used in mqtt topic
+# get device info - may be used in mqtt topic
 hostname = socket.gethostname()
-
+serial = subprocess.check_output('cat /proc/cpuinfo|grep Serial', shell=True).split(':')[1].strip()
+json_string = {'hostname': hostname, 'serial' : serial}
 
 def check_used_space(path):
-		used_space= subprocess.Popen("df -h / |tail +2| awk '{print $5}'", shell=True, stdout=subprocess.PIPE).communicate()[0]
-		used_space = used_space[:-1]
-		return used_space
+	return int(''.join(filter(str.isdigit, subprocess.Popen("df -h "+path+" |tail +2| awk '{print $5}'", shell=True, stdout=subprocess.PIPE).communicate()[0])))
 
 def check_cpu_info():
-		# bash command to get cpu load from uptime command
-		p = subprocess.Popen("uptime", shell=True, stdout=subprocess.PIPE).communicate()[0]
-		cores = int(subprocess.Popen("nproc", shell=True, stdout=subprocess.PIPE).communicate()[0])
-		cpu_load = p.split("average:")[1].split(",")[0].replace(' ', '')
-		cpu_load = float(cpu_load)/cores*100
-		cpu_load = round(float(cpu_load), 1)
-		p=p.split(",")
-		avr=p[2].split(": ")
-		avr1=float(avr[1])
-		avr5=float(p[3])
-		avr15=float(p[4])
-		up=p[0].split("up")
-		print up
-		uptime=up[1].strip()
-		return [cpu_load,cores,avr1,avr5,avr15,uptime]
+	# bash command to get cpu load from uptime command
+	avr = subprocess.Popen("uptime", shell=True, stdout=subprocess.PIPE).communicate()[0]
+	avr = avr.split("average:")[1].split(",")
+	cores = int(subprocess.Popen("nproc", shell=True, stdout=subprocess.PIPE).communicate()[0])
+	avr1 = float(avr[0])
+	avr5 = float(avr[1])
+	avr15 = float(avr[2])
+	cpu_load = float(avr1)/cores*100
+	cpu_load = round(float(cpu_load), 1)
+	return [cpu_load, cores, avr1, avr5, avr15]
 
-def check_cpu_average():
-		full_cmd = "cat /proc/loadavg|cut -d' ' -f1-3"
-		cpu_average = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
-		return cpu_average
-		
+def check_uptime():
+	# bash command to get cpu load from uptime command
+	uptime = subprocess.Popen("uptime -p", shell=True, stdout=subprocess.PIPE).communicate()[0]
+	uptime = uptime.split("up ")[1].strip("\n")
+	uptime = uptime.replace("hour", "heure")
+	uptime = uptime.replace("day", "jour")
+	uptime = uptime.replace("year", "ann\xc3e")
+	#print uptime
+	return uptime
+
 def check_voltage():
-		full_cmd = "vcgencmd measure_volts | cut -f2 -d= | sed 's/000//'"
-		voltage = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
-		voltage = voltage.strip()[:-1]
-		return voltage
+	full_cmd = "vcgencmd measure_volts | cut -f2 -d= | sed 's/000//'"
+	voltage = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
+	voltage = voltage.strip()[:-1]
+	return voltage
 
 def check_swap():
-		full_cmd = "free -t | awk 'NR == 3 {print $3/$2*100}'"
-		swap = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
-		swap = round(float(swap), 1)
-		return swap
+	full_cmd = "free -t | awk 'NR == 3 {print $3/$2*100}'"
+	swap = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
+	swap = round(float(swap), 1)
+	return swap
 
 def check_memory():
-		full_cmd = "free -t | awk 'NR == 2 {print $3/$2*100}'"
-		memory = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
-		memory = round(float(memory), 1)
-		return memory		
-		
+	full_cmd = "free -t | awk 'NR == 2 {print $3/$2*100}'"
+	memory = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
+	memory = round(float(memory), 1)
+	return memory		
+
 def check_cpu_temp():
-		full_cmd = "vcgencmd measure_temp"
-		p = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
-		cpu_temp = p.replace('\n', ' ').replace('\r', '').split("=")[1].split("'")[0]
-		return cpu_temp
-		
+	full_cmd = "vcgencmd measure_temp"
+	p = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
+	cpu_temp = p.replace('\n', ' ').replace('\r', '').split("=")[1].split("'")[0]
+	return cpu_temp
+
 def check_sys_clock_speed():
-		full_cmd = "awk '{printf (\"%0.0f\",$1/1000); }' </sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"
-		return subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
+	full_cmd = "awk '{printf (\"%0.0f\",$1/1000); }' </sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"
+	return subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
 
-	
-def publish_to_mqtt (cpu_info = [0,0,0,0,0,0], cpu_temp = 0, used_space = 0, voltage = 0, sys_clock_speed = 0, swap = 0, memory = 0):
-		# connect to mqtt server
-		client = paho.Client()
-		client.username_pw_set(config.mqtt_user, config.mqtt_password)
-		client.connect(config.mqtt_host, config.mqtt_port)
+def check_throttled():
+	MESSAGES = {
+		0: 'Under-voltage!',
+		1: 'ARM frequency capped!',
+		2: 'Currently throttled!',
+		3: 'Soft temperature limit active',
+		16: 'Under-voltage has occurred since last reboot.',
+		17: 'Throttling has occurred since last reboot.',
+		18: 'ARM frequency capped has occurred since last reboot.',
+		19: 'Soft temperature limit has occurred'
+	}
+	throttled_output = subprocess.check_output('vcgencmd get_throttled', shell=True)
+	throttled_binary = bin(int(throttled_output.split('=')[1], 0))
+	warnings = 0
+	messages = []
+	for position, message in MESSAGES.iteritems():
+	# Check for the binary digits to be "on" for each warning message
+		if len(throttled_binary) > position and throttled_binary[0 - position - 1] == '1':
+			#print(message)
+			messages.append(message)
+			warnings += 1
+	return [warnings, messages]
 
-		# publish monitored values to MQTT
-		if config.cpu_load:
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/cpuload", cpu_info[0], qos=1)
-			time.sleep(config.sleep_time)
-		if config.cores:
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/cores", cpu_info[1], qos=1)
-			time.sleep(config.sleep_time)
-		if config.cpu_average:
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/cpu1m", cpu_info[2], qos=1)
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/cpu5m", cpu_info[3], qos=1)
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/cpu15m", cpu_info[4], qos=1)
-			time.sleep(config.sleep_time)
-		if config.uptime:
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/uptime", cpu_info[5], qos=1)
-			time.sleep(config.sleep_time)
-		if config.cpu_temp:
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/cputemp", cpu_temp, qos=1)
-			time.sleep(config.sleep_time)
-		if config.used_space:
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/diskusage", used_space, qos=1)
-			time.sleep(config.sleep_time)
-		if config.voltage:
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/voltage", voltage, qos=1)
-			time.sleep(config.sleep_time)
-		if config.swap:
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/swap", swap, qos=1)
-			time.sleep(config.sleep_time)
-		if config.memory:
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/memory", memory, qos=1)
-			time.sleep(config.sleep_time)
-		if config.sys_clock_speed:
-			client.publish(config.mqtt_topic_prefix+"/"+hostname+"/sys_clock_speed", sys_clock_speed, qos=1)
-			time.sleep(config.sleep_time)
-		# disconect from mqtt server
-		client.disconnect()
-
-def bulk_publish_to_mqtt (cpu_info = [0,0,0,0,0,0], cpu_temp = 0, used_space = 0, voltage = 0, sys_clock_speed = 0, swap = 0, memory = 0):
-		# compose the CSV message containing the measured values
-		
-		values = cpu_info, float(cpu_temp), used_space, float(voltage), int(sys_clock_speed), swap, memory
-		values = str(values)[1:-1]
-
-		# connect to mqtt server
-		client = paho.Client()
-		client.username_pw_set(config.mqtt_user, config.mqtt_password)
-		client.connect(config.mqtt_host, config.mqtt_port)
-		
-		# publish monitored values to MQTT
-		client.publish(config.mqtt_topic_prefix+"/"+hostname, values, qos=1)
-		
-		# disconect from mqtt server
-		client.disconnect()
-
+def build_json():
+	cpu_info = check_cpu_info()
+	if config.cpu_load:
+		json_string['cpuload'] = cpu_info[0]
+	if config.cpu_average:
+		json_string['cpu1m'] = cpu_info[2]
+		json_string['cpu5m'] = cpu_info[3]
+		json_string['cpu15m'] = cpu_info[4]
+	if config.cores:
+		json_string['cores'] = cpu_info[1]
+	if config.uptime:
+		json_string['uptime'] = check_uptime()
+	if config.cpu_temp:
+		json_string['cputemp'] = check_cpu_temp()
+	if config.used_space:
+		json_string['diskusage'] = check_used_space('/')
+	if config.sys_clock_speed:
+		json_string['clockspeed'] = check_sys_clock_speed()
+	if config.voltage:
+		json_string['voltage'] = check_voltage()
+	if config.swap:
+		json_string['swap'] = check_swap()
+	if config.memory:
+		json_string['memory'] = check_memory()
+	if config.throttled:
+		throttled = check_throttled()
+		if throttled[0] == 0:
+			json_string['throttled'] = throttled[0]
+		else:
+			json_string['throttled'] = throttled[1]
 
 
 if __name__ == '__main__':
-		# set all monitored values to False in case they are turned off in the config
-		cpu_load = cores = cpu_average = cpu_temp = used_space = voltage = sys_clock_speed = swap = memory = False
-		
-		# delay the execution of the script
-		time.sleep(config.random_delay)
-		
-		# collect the monitored values		
-		if config.cpu_load or config.uptime or config.cpu_average or config.uptime:
-			cpu_info = check_cpu_info()
-		if config.cpu_temp:
-			cpu_temp = check_cpu_temp()
-		if config.used_space:
-			used_space = check_used_space('/')
-		if config.voltage:
-			voltage = check_voltage() 
-		if config.sys_clock_speed:
-			sys_clock_speed = check_sys_clock_speed()
-		if config.swap:
-			swap = check_swap()
-		if config.memory:
-			memory = check_memory()
-
-		# Publish messages to MQTT
-		if config.group_messages:
-			bulk_publish_to_mqtt(cpu_info, cpu_temp, used_space, voltage, sys_clock_speed, swap, memory)
-		else:
-			publish_to_mqtt(cpu_info, cpu_temp, used_space, voltage, sys_clock_speed, swap, memory)
+	# delay the execution of the script
+	time.sleep(config.random_delay)
+	#print config.mqtt_topic_prefix
+	# connect to mqtt server
+	client = paho.Client(serial)
+	client.username_pw_set(config.mqtt_user, config.mqtt_password)
+	client.connect(config.mqtt_host, config.mqtt_port)
+	# Publish messages to MQTT
+	build_json()
+	client.publish(config.mqtt_topic_prefix+"/"+hostname, json.dumps(json_string), 1)
+	time.sleep(config.sleep_time)
+	# disconnect from mqtt server
+	client.disconnect()
+	#print json_string
